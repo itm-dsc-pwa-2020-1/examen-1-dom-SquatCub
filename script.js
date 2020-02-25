c148acba3475cc51cfb538fe97b304af83189d6e
SquatCub <brandon.alexis10@hotmail.com>
2020-02-25 13:58:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function dark() {
+  document.getElementById('head').style.color = "#FFF";
+  document.getElementById('head').style.background = "#000";
+  document.getElementById('middle').style.background = "#2E2E2E";
+  document.getElementById('desc').style.color = "#fff";
+  document.getElementById('social').style.background = "#4C4C4C";
+  document.getElementById('botones').style.background = "#4C4C4C";
+  document.getElementById('imagenes').style.background = "#4C4C4C";
+  document.querySelector('body').style.background="#4C4C4C";
+
+}
+
+function light(){
+  document.getElementById('head').style.color = "#000";
+  document.getElementById('head').style.background = "#FFF";
+  document.getElementById('middle').style.background = "#FFF";
+  document.getElementById('desc').style.color = "#000";
+  document.getElementById('social').style.background = "#FFF";
+  document.getElementById('botones').style.background = "#FFF";
+  document.getElementById('imagenes').style.background = "#FFF";
+  document.querySelector('body').style.background="#fff";
+}
